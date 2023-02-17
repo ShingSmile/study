71ac3a700b4f65f4f963aece93e0d1635eff6cb7
祁 <1844551387@qq.com>
2023-02-17 09:16:42 +0800

    redis缓存验证码

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -7,6 +7,7 @@ import com.study.common.User;
 import com.study.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -22,6 +24,9 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
     @PostMapping("/sendMsg")
@@ -33,8 +38,9 @@ public class UserController {
             log.info(code);
             //这里的phone其实就是邮箱，code是我们生成的验证码
             MailUtils.sendTestMail(phone, code);
-            //验证码存session，方便后面拿出来比对
-            session.setAttribute(phone, code);
+//            //验证码存session，方便后面拿出来比对
+//            session.setAttribute(phone, code);
+            redisTemplate.opsForValue().set("code",code,5, TimeUnit.MINUTES);
             return Result.success("验证码发送成功");
         }
         return Result.error("验证码发送失败");
@@ -47,10 +53,12 @@ public class UserController {
         String phone = map.get("phone").toString();
         //获取验证码
         String code = map.get("code").toString();
-        //从session中获取验证码
-        String codeInSession = session.getAttribute(phone).toString();
+//        //从session中获取验证码
+//        String codeInSession = session.getAttribute(phone).toString();
+        Object codeInRedis = redisTemplate.opsForValue().get("code");
+        log.info("你输入的code{}，redis中的code{}，计算结果为{}", code, codeInRedis, (code != null && code.equals(codeInRedis)));
         //比较这用户输入的验证码和session中存的验证码是否一致
-        if (code != null && code.equals(codeInSession)) {
+        if (code != null && code.equals(codeInRedis)) {
             //如果输入正确，判断一下当前用户是否存在
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             //判断依据是从数据库中查询是否有其邮箱
@@ -61,10 +69,12 @@ public class UserController {
                 user = new User();
                 user.setPhone(phone);
                 userService.save(user);
-                user.setName("用户" + codeInSession);
+                user.setName("用户" + codeInRedis);
             }
             //存个session，表示登录状态
             session.setAttribute("user",user.getId());
+            //如果登录成功，则删除Redis中的验证码
+            redisTemplate.delete(phone);
             //并将其作为结果返回
             return Result.success(user);
         }
